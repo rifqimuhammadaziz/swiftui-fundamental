959b089deb97de8c3d15a62383ab786a169ce55d
rifqimuhammadaziz <rifqimuhammadaziz@gmail.com>
2023-01-07 12:31:35 +0700

    add inits & enums

--- SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/InitializerPractice.swift
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/InitializerPractice.swift
@@ -9,15 +9,40 @@ import SwiftUI
 
 struct InitializerPractice: View {
     
-    let backgroundColor: Color = Color.blue
+    let backgroundColor: Color
+    let count: Int
+    let title: String
+    
+    init(count: Int, fruit: Fruit) {
+        self.count = count
+//        self.title = title
+//
+//        if title == "Apples" {
+//            self.backgroundColor = Color.red
+//        } else {
+//            self.backgroundColor = Color.blue
+//        }
+        if fruit == .apple {
+            self.title = "Apples"
+            self.backgroundColor = Color.red
+        } else {
+            self.title = "Orange"
+            self.backgroundColor = Color.orange
+        }
+    }
+    
+    enum Fruit {
+        case apple
+        case orange
+    }
     
     var body: some View {
         VStack(spacing: 12) {
-            Text("5")
+            Text("\(count)")
                 .font(.largeTitle)
                 .underline()
             
-            Text("Mangos")
+            Text(title)
                 .font(.headline)
         }
         .foregroundColor(Color.white)
@@ -29,6 +54,9 @@ struct InitializerPractice: View {
 
 struct InitializerPractice_Previews: PreviewProvider {
     static var previews: some View {
-        InitializerPractice()
+        HStack {
+            InitializerPractice(count: 20, fruit: .apple)
+            InitializerPractice(count: 100, fruit: .orange)
+        }
     }
 }
