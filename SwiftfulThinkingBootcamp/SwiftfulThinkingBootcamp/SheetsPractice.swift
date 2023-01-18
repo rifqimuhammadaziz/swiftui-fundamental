ac06acf792ee52d1ac7830cf5889fad9f79841d9
rifqimuhammadaziz <rifqimuhammadaziz@gmail.com>
2023-01-18 14:04:33 +0700

    display pop up sheet and fullscreen covers

---
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/SheetsPractice.swift
@@ -0,0 +1,20 @@
+//
+//  SheetsPractice.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Rifqi Muhammad Aziz on 18/01/23.
+//
+
+import SwiftUI
+
+struct SheetsPractice: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SheetsPractice_Previews: PreviewProvider {
+    static var previews: some View {
+        SheetsPractice()
+    }
+}
