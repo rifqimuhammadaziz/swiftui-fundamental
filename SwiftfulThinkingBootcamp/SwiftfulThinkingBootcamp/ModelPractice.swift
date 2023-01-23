e8d19012f91ba3f4ce1264449197a9887de74816
rifqimuhammadaziz <rifqimuhammadaziz@gmail.com>
2023-01-23 09:28:38 +0700

    update custom models

--- SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/ModelPractice.swift
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/ModelPractice.swift
@@ -12,16 +12,17 @@ struct UserModel: Identifiable {
     let displayName: String
     let username: String
     let followerCount: Int
+    let isVerified: Bool
 }
 
 struct ModelPractice: View {
     
     @State var users: [UserModel] = [
         //        "Andreas", "Heldy", "Frozenost", "Xenosty", "Christopher"
-        UserModel(displayName: "Rifqi Muhammad", username: "rifqimuhammad", followerCount: 581),
-        UserModel(displayName: "Xenosty Theord", username: "xenosty", followerCount: 991),
-        UserModel(displayName: "Bagas Dwi", username: "anthos", followerCount: 256),
-        UserModel(displayName: "Rizqi Virgiawan", username: "yellowflash", followerCount: 872)
+        UserModel(displayName: "Rifqi Muhammad", username: "rifqimuhammad", followerCount: 581, isVerified: true),
+        UserModel(displayName: "Xenosty Theord", username: "xenosty", followerCount: 991, isVerified: false),
+        UserModel(displayName: "Bagas Dwi", username: "anthos", followerCount: 256, isVerified: true),
+        UserModel(displayName: "Rizqi Virgiawan", username: "yellowflash", followerCount: 872, isVerified: false)
     ]
     
     var body: some View {
@@ -39,7 +40,27 @@ struct ModelPractice: View {
                     HStack(spacing: 15) {
                         Circle()
                             .frame(width: 35, height: 35)
-                        Text(user.id)
+                        VStack(alignment: .leading) {
+                            Text(user.displayName)
+                            Text("@\(user.username)")
+                                .foregroundColor(.gray)
+                                .font(.caption)
+                        }
+                        
+                        Spacer()
+                        
+                        if user.isVerified {
+                            Image(systemName: "checkmark.seal.fill")
+                                .foregroundColor(.blue)
+                        }
+                        
+                        VStack {
+                            Text("\(user.followerCount)")
+                                .font(.headline)
+                            Text("Followers")
+                                .foregroundColor(.gray)
+                                .font(.caption)
+                        }
                     }
                     .padding(.vertical, 10)
                 }
